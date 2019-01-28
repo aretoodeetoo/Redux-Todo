586ecfe17f4541160b0ee2e7b9f2c88605e973f6
croy <roy.christie95@gmail.com>
2019-01-28 17:05:02 -0600

    Created TodoList and TodoItem components

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import TodoItem from './TodoItem';
+
 const TodoList = props => {
     return (
         <div>
         <h1>Hello, World!</h1>
+        {props.todos.map(todo => {
+            return <TodoItem 
+            todo={todo}
+            />
+        })}
         </div>
     );
 }
