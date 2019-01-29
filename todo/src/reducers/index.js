f1944bdc10afc92c8b3856fe1bd92c25519f1cca
croy <roy.christie95@gmail.com>
2019-01-29 14:41:29 -0600

    Updated reducers files with correct state and reducer functions, todo Items are now rendering

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,19 +1,20 @@
 import { ADD_NEW_ITEM } from '../actions';
 
 const initialState = {
-    todos: [],
-    todo: {
-        value: '',
-        completed: false
-    }
-}
+    todos: []
+};
 
 const reducer = (state = initialState, action) => {
+    console.log('reducer', action);
     switch (action.type){
         case ADD_NEW_ITEM:
+        const newTodo = {
+            value: action.payload,
+            completed: false
+        };
         return {
             ...state,
-            todo: {}
+            todos: [...state.todos, newTodo]
         }
         default:
         return state;
